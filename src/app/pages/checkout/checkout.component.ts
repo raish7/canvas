f3e47e51932228e0d885e2edee8de77e06c9d550
raish7 <harryfelixprince@gmail.com>
2024-12-25 08:47:41 +0545

    commented lines

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -8,7 +8,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { Router } from '@angular/router';
 import { toastMixin } from '../../utils/toastMixin';
 import { AuthService } from '../../services/auth/auth.service';
-import CryptoJS from 'crypto-js';
+// import CryptoJS from 'crypto-js';
 
 // import SHA256 from "crypto-js/sha256";
 // import HmacMD5 from "crypto-js/hmac-md5";
@@ -16,8 +16,8 @@ import CryptoJS from 'crypto-js';
 // import Base64 from "crypto-js/enc-base64";
 // import Utf8 from "crypto-js/enc-utf8";
 // import AES from "crypto-js/aes";
-import HmacSHA256 from 'crypto-js/hmac-sha256';
-import Base64 from 'crypto-js/enc-base64';
+// import HmacSHA256 from 'crypto-js/hmac-sha256';
+// import Base64 from 'crypto-js/enc-base64';
 
 @Component({
   selector: 'app-checkout',
@@ -67,11 +67,6 @@ export class CheckoutComponent {
         this.checkoutItems.set([...res]);
       })
     );
-    const signedData = `total_amount=100,transaction_uuid=2081-82X2,product_code=EPAYTEST`;
-    // Generate the HMAC-SHA256 hash
-    const hash = CryptoJS.HmacSHA256(signedData, this.esewaKey);
-    const hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
-    console.log('hash', hashInBase64);
   }
 
   purchase() {
@@ -136,53 +131,53 @@ export class CheckoutComponent {
           });
         break;
       case 'esewa':
-        // Concatenate the fields in the order specified
-        const signedData = `total_amount=100,transaction_uuid=2081-82X2,product_code=EPAYTEST`;
-        // Generate the HMAC-SHA256 hash
-        const hash = CryptoJS.HmacSHA256(signedData, this.esewaKey);
-        const hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
-        console.log('hash', hashInBase64);
+        // // Concatenate the fields in the order specified
+        // const signedData = `total_amount=100,transaction_uuid=2081-82X2,product_code=EPAYTEST`;
+        // // Generate the HMAC-SHA256 hash
+        // const hash = CryptoJS.HmacSHA256(signedData, this.esewaKey);
+        // const hashInBase64 = CryptoJS.enc.Base64.stringify(hash);
+        // console.log('hash', hashInBase64);
 
-        const payload = {
-          amount: 100,
-          tax_amount: 0,
-          product_service_charge: 0,
-          product_delivery_charge: 0,
-          product_code: 'EPAYTEST',
-          total_amount: 100,
-          transaction_uuid: '2081-82X2',
-          success_url: 'http://localhost:4200/orders',
-          failure_url: 'http://localhost:4200',
-          signed_field_names: 'total_amount,transaction_uuid,product_code',
-          signature: hashInBase64,
-        };
-        const formData = new FormData();
-        formData.append('amount', payload.amount.toString());
-        formData.append('tax_amount', payload.tax_amount.toString());
-        formData.append(
-          'product_service_charge',
-          payload.product_service_charge.toString()
-        );
-        formData.append(
-          'product_delivery_charge',
-          payload.product_delivery_charge.toString()
-        );
-        formData.append('product_code', payload.product_code);
-        formData.append('total_amount', payload.total_amount.toString());
-        formData.append('transaction_uuid', payload.transaction_uuid);
-        formData.append('success_url', payload.success_url);
-        formData.append('failure_url', payload.failure_url);
-        formData.append('signed_field_names', payload.signed_field_names);
-        formData.append('signature', payload.signature);
-        this.paymentService.esewaPayment(formData).subscribe({
-          next: (res: any) => {
-            console.log('ressss', res);
-          },
-          error: (err: any) => {
-            console.log('err', err);
+        // const payload = {
+        //   amount: 100,
+        //   tax_amount: 0,
+        //   product_service_charge: 0,
+        //   product_delivery_charge: 0,
+        //   product_code: 'EPAYTEST',
+        //   total_amount: 100,
+        //   transaction_uuid: '2081-82X2',
+        //   success_url: 'http://localhost:4200/orders',
+        //   failure_url: 'http://localhost:4200',
+        //   signed_field_names: 'total_amount,transaction_uuid,product_code',
+        //   signature: hashInBase64,
+        // };
+        // const formData = new FormData();
+        // formData.append('amount', payload.amount.toString());
+        // formData.append('tax_amount', payload.tax_amount.toString());
+        // formData.append(
+        //   'product_service_charge',
+        //   payload.product_service_charge.toString()
+        // );
+        // formData.append(
+        //   'product_delivery_charge',
+        //   payload.product_delivery_charge.toString()
+        // );
+        // formData.append('product_code', payload.product_code);
+        // formData.append('total_amount', payload.total_amount.toString());
+        // formData.append('transaction_uuid', payload.transaction_uuid);
+        // formData.append('success_url', payload.success_url);
+        // formData.append('failure_url', payload.failure_url);
+        // formData.append('signed_field_names', payload.signed_field_names);
+        // formData.append('signature', payload.signature);
+        // this.paymentService.esewaPayment(formData).subscribe({
+        //   next: (res: any) => {
+        //     console.log('ressss', res);
+        //   },
+        //   error: (err: any) => {
+        //     console.log('err', err);
             
-          },
-        });
+        //   },
+        // });
     }
   }
 
